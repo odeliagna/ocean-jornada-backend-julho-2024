30cff07e72987efc5b161797d8049c557c5773f7
odeliagna <124363502+odeliagna@users.noreply.github.com>
2024-07-03 14:03:36 +0000

    EndPoint create finalizado

--- index.js
+++ index.js
@@ -5,4 +5,28 @@ app.get('/', function (req, res) {
   res.send('Aloha, Mundão')
 })
 
+//Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read All - GET /item
+app.get('/item', function (req, res) {
+  // Pegamos a lista e enviamos como resposta ao http
+  res.send(lista)
+})
+
+//Sinalizamos para o Express que vamos usar JSON no Body
+app.use(express.json())
+
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+  // Obtemos o nome enviado no Request Body
+  const item = req.body.nome
+
+  //Inserimos o item no final da lista
+  lista.push(item)
+
+  //Enviamos uma mensagem de sucesso!
+  res.send('Item criado com sucesso!')
+})
+
 app.listen(3000)
